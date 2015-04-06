2aaff18ff5d5e66ea424402245d6b54cbefeb6fb
Youssef Hatem <youssef.hatem@sap.com>
2015-04-06 14:16:07 +0200

    * Configurability of hierarchy implementation
    + unit tests
    
    Change-Id: I5570130a5fb556b97999f00b53f09eb2a364b0b5

--- src/main/scala/org/apache/spark/sql/hierarchy/HierarchyStrategy.scala
+++ src/main/scala/org/apache/spark/sql/hierarchy/HierarchyStrategy.scala
@@ -1,22 +1,46 @@
 package org.apache.spark.sql.hierarchy
 
+import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Row
-import org.apache.spark.sql.catalyst.expressions.{SortOrder, Expression, Attribute}
+import org.apache.spark.sql.catalyst.expressions.{Attribute, Expression, SortOrder}
 
 case class HierarchyStrategy(attributes: Seq[Attribute], parenthoodExpression: Expression,
                              startWhere: Expression, searchBy: Seq[SortOrder]) {
-  /* TODO (YH, SM) make this configurable at least for tests. */
-  val threshold = 7L
+
+  private def parseConfig(sc : SparkContext) : (String, Long) = {
+    val impName = sc.conf.get("hierarchy.always", "undefined")
+    val actualThreshold = sc.conf.get("hierarchy.threshold",
+                                      HierarchyStrategy.THRESHOLD.toString).toLong
+    if(actualThreshold < 0) throw new IllegalArgumentException("threshold must be non-negative")
+    impName match  {
+      case "join" | "broadcast" | "undefined" => (impName, actualThreshold)
+      case _ => throw new IllegalArgumentException("unknown implementation: " + impName)
+    }
+  }
+
+  private def useBroadcastHierarchy(conf : (String, Long), count: => Long) : Boolean = {
+    /* TODO (YH) delay the calculation of rows until it is very necessary */
+    lazy val cnt = count
+    conf match {
+      case ("broadcast", _) => true
+      case ("join", _) => false
+      case ("undefined", threshold) if cnt < threshold => true
+      case ("undefined", threshold) if cnt  >= threshold => false
+      case _ => throw new IllegalStateException("Unknown configuration" + conf)
+    }
+  }
 
   def execute(rdd: RDD[Row]): RDD[Row] = {
-    if (rdd.count() < threshold) {
-      HierarchyRowBroadcastBuilder(attributes, parenthoodExpression,
-        startWhere, searchBy)
-        .buildFromAdjacencyList(rdd)
-    } else {
-      HierarchyRowJoinBuilder(attributes, parenthoodExpression, startWhere, searchBy)
+    useBroadcastHierarchy(parseConfig(rdd.sparkContext), rdd count) match {
+      case true => HierarchyRowBroadcastBuilder(attributes, parenthoodExpression, startWhere,
+                                                searchBy).buildFromAdjacencyList(rdd)
+      case false => HierarchyRowJoinBuilder(attributes, parenthoodExpression, startWhere, searchBy)
         .buildFromAdjacencyList(rdd)
     }
   }
+}
+
+object HierarchyStrategy {
+  val THRESHOLD = 1000000L
 }
---
+++ src/test/scala/org/apache/spark/sql/hierarchy/HierarchyStrategySuite.scala
@@ -0,0 +1,79 @@
+package org.apache.spark.sql.hierarchy
+
+import corp.sap.spark.SharedSparkContext
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{catalyst, Row, VelocitySQLContext}
+import org.scalatest.{PrivateMethodTester, FunSuite}
+import org.apache.spark.sql.catalyst.expressions.{Expression, SortOrder, Attribute}
+import org.scalatest.PrivateMethodTester._
+
+import scala.util.Random
+
+// scalastyle:off magic.number
+
+class HierarchyStrategySuite extends FunSuite with SharedSparkContext {
+
+  test("unit: test hierarchy strategy with default context configuration") {
+    val strategy = new HierarchyStrategy(Nil, null, null, Nil)
+    val parseConfigMethod = PrivateMethod[SparkContext]('parseConfig)
+    val expected = ("undefined", HierarchyStrategy.THRESHOLD)
+    assertResult(expected)(strategy invokePrivate parseConfigMethod(sc))
+  }
+
+  test("unit: test hierarchy strategy with valid context configuration") {
+    val strategy = new HierarchyStrategy(Nil, null, null, Nil)
+    val parseConfigMethod = PrivateMethod[SparkContext]('parseConfig)
+    sc.conf.set("hierarchy.always", "broadcast")
+    var expected = ("broadcast", HierarchyStrategy.THRESHOLD)
+    assertResult(expected)(strategy invokePrivate parseConfigMethod(sc))
+    sc.conf.set("hierarchy.always", "join")
+    expected = ("join", HierarchyStrategy.THRESHOLD)
+    assertResult(expected)(strategy invokePrivate parseConfigMethod(sc))
+    sc.conf.set("hierarchy.always", "undefined")
+    sc.conf.set("hierarchy.threshold", "10000")
+    expected = ("undefined", 10000L)
+    assertResult(expected)(strategy invokePrivate parseConfigMethod(sc))
+  }
+
+  test("unit: setting invalid context configuration for hierarchy strategy throws") {
+    val strategy = new HierarchyStrategy(Nil, null, null, Nil)
+    val parseConfigMethod = PrivateMethod[SparkContext]('parseConfig)
+    sc.conf.set("hierarchy.threshold", "-100")
+    intercept[IllegalArgumentException]{
+      strategy invokePrivate parseConfigMethod(sc)
+    }
+    sc.conf.set("hierarchy.threshold", "1000")
+    sc.conf.set("hierarchy.always", "invalid")
+    intercept[IllegalArgumentException]{
+      strategy invokePrivate parseConfigMethod(sc)
+    }
+  }
+
+  test("unit: below threshold show use broadcast hierarchy") {
+    val strategy = new HierarchyStrategy(Nil, null, null, Nil)
+    val getBuilderMethod = PrivateMethod[Boolean]('useBroadcastHierarchy)
+    val actual = strategy invokePrivate getBuilderMethod(("undefined", 10L),  () => 9L)
+    assertResult(true)(actual)
+  }
+
+  test("unit: above or equal threshold show use join hierarchy") {
+    val strategy = new HierarchyStrategy(Nil, null, null, Nil)
+    val getBuilderMethod = PrivateMethod[Boolean]('useBroadcastHierarchy)
+    var actual = strategy invokePrivate getBuilderMethod(("undefined", 10L),  () => 11L)
+    assertResult(false)(actual)
+    actual = strategy invokePrivate getBuilderMethod(("undefined", 10L),  () => 10L)
+    assertResult(false)(actual)
+  }
+
+  test("unit: explicit implementation in config should override threshold") {
+    val strategy = new HierarchyStrategy(Nil, null, null, Nil)
+    val getBuilderMethod = PrivateMethod[Boolean]('useBroadcastHierarchy)
+    var actual = strategy invokePrivate getBuilderMethod(("broadcast", 10L),  () => 11L)
+    assertResult(true)(actual)
+    actual = strategy invokePrivate getBuilderMethod(("broadcast", 10L),  () => 10L)
+    assertResult(true)(actual)
+    actual = strategy invokePrivate getBuilderMethod(("join", 10L),  () => 9L)
+    assertResult(false)(actual)
+  }
+}
