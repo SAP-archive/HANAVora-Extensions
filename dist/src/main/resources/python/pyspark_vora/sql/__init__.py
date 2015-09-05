c8dc20677d92feb6c0f7f0bf29c369269d07c1d6
Santosh Addanki <santosh.kumar.addanki@sap.com>
2015-09-04 18:12:14 -0700

    Python Binding for SapSQLContext
    -Added the python package to the extensions package
    -Adjusted the platform assembly to include the python fileset
    -Refactored the classes to use SapSQLContext
    -Added the python test cases
    -exec-maven-plugin for python tests
    -Remove the dependency to SPARK_CLASSPATH
    -Adjust the shell script to fail on SPARK_HOME not set
    -Unset JAVA_HOME
    
    Change-Id: I8b7ee0edd04b79dd9519e9b12c484448860bd420

---
+++ dist/src/main/resources/python/pyspark_vora/sql/__init__.py
@@ -0,0 +1,11 @@
+"""
+Important classes of Spark SQL and DataFrames using SAP VORA:
+
+    - L{SapSQLContext}
+      Main entry point for :class:`DataFrame` and SQL functionality.
+
+"""
+
+from pyspark_vora.sql.context import SapSQLContext
+
+__a__ = ['SapSQLContext']
