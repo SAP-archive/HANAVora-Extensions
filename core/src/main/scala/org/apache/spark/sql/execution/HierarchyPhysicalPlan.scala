50c18e01d1c4f2a2f51d2684f12f0b54ff0cefb8
Santiago M. Mola <santiago.mola@sap.com>
2015-09-19 16:43:46 +0200

    Disable hierarchy join builder.
    - Disable hierarchy join builder (will be back once
      it is stable).
    - Removed threshold logic to choose hierarchy implementation. This
      will avoid the intermediate count. This will have to be reconsidered
      once we get the join builder back.
    
    Change-Id: I1c2d3550ebaa69b69c6e3fd9932a1805ca8827b5

--- core/src/main/scala/org/apache/spark/sql/execution/HierarchyPhysicalPlan.scala
+++ core/src/main/scala/org/apache/spark/sql/execution/HierarchyPhysicalPlan.scala
@@ -2,8 +2,10 @@ package org.apache.spark.sql.execution
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Row
+import org.apache.spark.sql.catalyst.CatalystTypeConverters
 import org.apache.spark.sql.catalyst.expressions.{Attribute, Expression, SortOrder}
-import org.apache.spark.sql.hierarchy.HierarchyStrategy
+import org.apache.spark.sql.hierarchy.HierarchyRowBroadcastBuilder
+import org.apache.spark.sql.types.{Node, NodeType, StructField, StructType}
 
 case class HierarchyPhysicalPlan(childAlias: String,
                                  parenthoodExpression: Expression,
@@ -12,16 +14,38 @@ case class HierarchyPhysicalPlan(childAlias: String,
                                  nodeAttribute: Attribute,
                                  child: SparkPlan) extends UnaryNode {
 
-  override def doExecute(): RDD[Row] = HierarchyStrategy(
-    child,
-    child.output,
-    parenthoodExpression,
-    startWhere,
-    searchBy
-  ).execute(child.execute())
-
   override def output: Seq[Attribute] = child.output :+ nodeAttribute
 
+  override def doExecute(): RDD[Row] = {
+    val rdd = child.execute()
+    val attributes = child.output
 
+    /* XXX: Copied from DataFrame. See SPARK-4775 (weird duplicated rows). */
+    val childSchema = child.schema
+    val cachedRdd = rdd.mapPartitions({ iter =>
+      val converter = CatalystTypeConverters.createToScalaConverter(childSchema)
+      iter.map(converter(_).asInstanceOf[Row])
+    }).cache()
+    val resultRdd = HierarchyRowBroadcastBuilder(
+      attributes,
+      parenthoodExpression,
+      startWhere,
+      searchBy
+    ).buildFromAdjacencyList(cachedRdd)
+    val cachedResultRdd = resultRdd
+      .mapPartitions({ iter =>
+      val schemaWithNode =
+        StructType(childSchema.fields ++ Seq(StructField("", NodeType, nullable = false)))
+      val converter = CatalystTypeConverters.createToCatalystConverter(schemaWithNode)
+      iter.map({ row =>
+        val node = row.getAs[Node](row.length - 1)
+        val rowWithoutNode = Row(row.toSeq.take(row.length - 1): _*)
+        val convertedRowWithoutNode = converter(rowWithoutNode).asInstanceOf[Row]
+        Row.fromSeq(convertedRowWithoutNode.toSeq :+ node)
+      })
+    }).cache()
+    cachedRdd.unpersist(blocking = false)
+    cachedResultRdd
+  }
 
 }
