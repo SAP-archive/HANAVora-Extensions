ea3c9efcab4adff3f1f02dbffef745f5c06c8cdc
Santiago M. Mola <santiago.mola@sap.com>
2015-12-08 13:17:05 +0100

    * Add Scaladoc and improve readability of AddSubqueries rule.
    Change-Id: I77eacb0c2b6025bf77bf2b32f62c56d86be4b77e

--- core/src/main/scala/org/apache/spark/sql/catalyst/analysis/AddSubqueries.scala
+++ core/src/main/scala/org/apache/spark/sql/catalyst/analysis/AddSubqueries.scala
@@ -7,71 +7,99 @@ import org.apache.spark.sql.sources.LogicalRelation
 import org.apache.spark.sql.sources.sql.{SingleQuery, SqlLikeRelation}
 
 /**
- * Adds subqueries where needed for SQL generation.
- */
+  * Adds subqueries where needed for SQL generation. This rule is used
+  * by [[org.apache.spark.sql.sources.CatalystSource]] implementations
+  * that need to generate a SQL query from a [[LogicalPlan]]. Adding
+  * subqueries eases to to create 1-to-1 mappings between [[LogicalPlan]]
+  * and SQL.
+  *
+  * For example:
+  *   Project(columns1, Project(columns2, myRelation))
+  * is translated to:
+  *   Project(columns1, Subquery("\_\_subquery1", Project(columns2, myRelation)))
+  * which eases the generation of the following SQL query:
+  *   SELECT columns1 FROM (SELECT columns2 FROM myRelation) AS "\_\_subquery1";
+  *
+  * Subqueries are introduced between any two [[LogicalPlan]] nodes where a direct
+  * SQL mapping is not possible.
+  *
+  * TODO: This rule might be moved to [[org.apache.spark.sql.sources.sql]].
+  *
+  * @see [[org.apache.spark.sql.sources.sql.SqlBuilder]]
+  */
 object AddSubqueries extends Rule[LogicalPlan] {
 
   // scalastyle:off cyclomatic.complexity
   // scalastyle:off method.length
   override def apply(plan: LogicalPlan): LogicalPlan = {
-    val planWithSubqueries = transform(plan)
-    planWithSubqueries transformUp {
-      case Subquery(name, Subquery(innerName, child)) =>
-        /* If multiple subqueries, preserve the inner one */
-        logDebug(s"Nested subqueries ($name, $innerName) -> $innerName")
-        Subquery(innerName, child)
-    } match {
-      /* Avoid outer subquery */
-      case Subquery(name, child) => child
-      case other => other
-    }
+    val planWithSubqueries = addSubqueries(plan)
+    removeRedundantSubqueries(planWithSubqueries)
   }
 
-  private def isSetOperation(lp: LogicalPlan): Boolean = lp match {
-    case _: Union | _: Except | _: Intersect => true
-    case _ => false
-  }
-
-  private[this] def getName(plan: LogicalPlan): Option[String] =
+  /**
+    * Main logic to add subqueries.
+    *
+    * @param plan Input [[LogicalPlan]].
+    * @param ids An ID generator, defaults to a fresh one.
+    * @return [[LogicalPlan]] with subqueries.
+    */
+  private[this] def addSubqueries(plan: LogicalPlan, ids: Iterator[Int] = newIds()): LogicalPlan =
     plan match {
-      case LogicalRelation(table: SqlLikeRelation) => Some(table.tableName)
-      case Subquery(alias, child) => Some(alias)
-      case _ => None
-    }
 
-  private def newIds(): Iterator[Int] = Stream.from(1).iterator
+      /** End recursion at leaves */
+      case relation@LogicalRelation(_: SqlLikeRelation) =>
+        relation
 
-  private[this] def newTableName(ids: Iterator[Int]): String = s"__table${ids.next()}"
+      /** Subqueries are preserved as they are */
+      case Subquery(alias, child) =>
+        Subquery(alias, addSubqueries(child, ids))
 
-  private[this] def newQueryName(ids: Iterator[Int]): String = s"__subquery${ids.next()}"
-
-  private[this] def transform(plan: LogicalPlan, ids: Iterator[Int] = newIds()): LogicalPlan =
-    plan match {
-      case relation@LogicalRelation(_: SqlLikeRelation) => relation
-      case Subquery(alias, child) => Subquery(alias, transform(child, ids))
+      /**
+        * Distinct(Union(left, right)) is equivalent to SQL UNION [DISTINCT].
+        * (not UNION ALL). So we avoid introducing subqueries between Distinct
+        * and Union nodes. Otherwise, it would lead to
+        * SELECT DISTINCT * FROM (left UNION ALL right);
+        */
       case Distinct(union@Union(left, right)) =>
-        Distinct(transform(union))
+        Distinct(addSubqueries(union))
+
+      /**
+        * On JOIN, we check left and right side:
+        *   - If a side has no name, we add a subquery.
+        *   - If a side has the same name as the other side, we add subquery.
+        *     This prevents conflicts on self-joins.
+        */
       case Join(left, right, joinType, condition) =>
         val leftName = getName(left)
         val rightName = getName(right)
         val newLeft = if (leftName.isEmpty || rightName == leftName) {
-          Subquery(newQueryName(ids), transform(left, ids))
+          Subquery(newQueryName(ids), addSubqueries(left, ids))
         } else {
-          transform(left, ids)
+          addSubqueries(left, ids)
         }
         val newRight = if (rightName.isEmpty || rightName == leftName) {
-          Subquery(newQueryName(ids), transform(right, ids))
+          Subquery(newQueryName(ids), addSubqueries(right, ids))
         } else {
-          transform(right, ids)
+          addSubqueries(right, ids)
         }
         Join(newLeft, newRight, joinType, condition)
+
+      /**
+        * If the plan is a hierarchy, we always add a subquery to the child.
+        */
       case hierarchy: Hierarchy =>
         val alias = newQueryName(ids)
         logTrace(s"Added subquery $alias to $hierarchy")
-        hierarchy.withNewChildren(Subquery(alias, transform(hierarchy.child, ids)) :: Nil)
+        hierarchy.withNewChildren(Subquery(alias, addSubqueries(hierarchy.child, ids)) :: Nil)
+
+      /**
+        * Use [[SingleQuery]] to get a subtree of the plan that can be represented
+        * as a SQL query without subqueries, set operations or joins. Then preserve it
+        * and add a subquery after its FROM clause.
+        */
       case SingleQuery(select, from, where, groupBy, _ /* TODO */, orderBy, limit, distinct)
         if from != plan =>
-        val fromPlan = transform(from, ids)
+        val fromPlan = addSubqueries(from, ids)
         val fromSubqueryPlan = if (getName(fromPlan).isEmpty && !fromPlan.isInstanceOf[Join]) {
           Subquery(newQueryName(ids), fromPlan)
         } else {
@@ -105,18 +133,68 @@ object AddSubqueries extends Rule[LogicalPlan] {
           orderByPlan
         }
         limitPlan
+
+      /**
+        * If children of the current node contain a set operation, we
+        * add subqueries to every children.
+        */
       case other if other.children.exists(isSetOperation) =>
         val newChildren = other.children map { child =>
           val alias = newQueryName(ids)
           logTrace(s"Added subquery $alias to $child")
-          Subquery(alias, transform(child, ids))
+          Subquery(alias, addSubqueries(child, ids))
         }
         other.withNewChildren(newChildren)
+
+      /**
+        * In any case that was not covered, do not add a subquery and
+        * continue the recursion with all children.
+        */
       case other =>
         val newChildren = other.children.map { child =>
-          transform(child, ids)
+          addSubqueries(child, ids)
         }
         other.withNewChildren(newChildren)
     }
+
+  /** Removes redundant subqueries */
+  private[this] def removeRedundantSubqueries(plan: LogicalPlan): LogicalPlan =
+    plan transformUp {
+      case Subquery(name, Subquery(innerName, child)) =>
+        /* If multiple subqueries, preserve the inner one */
+        logDebug(s"Nested subqueries ($name, $innerName) -> $innerName")
+        Subquery(innerName, child)
+    } match {
+      /* Avoid outer subquery */
+      case Subquery(name, child) => child
+      case other => other
+    }
+
+  /** Returns true if the input [[LogicalPlan]] is a set operation */
+  private[this] def isSetOperation(lp: LogicalPlan): Boolean = lp match {
+    case _: Union | _: Except | _: Intersect => true
+    case _ => false
+  }
+
+  /**
+    * Gets the name of a [[LogicalPlan]], if any. A name can be provided by:
+    *   - Subquery name.
+    *   - [[SqlLikeRelation]]
+    *
+    * @param plan Input [[LogicalPlan]].
+    * @return Name, if any.
+    */
+  private[this] def getName(plan: LogicalPlan): Option[String] =
+    plan match {
+      case LogicalRelation(table: SqlLikeRelation) => Some(table.tableName)
+      case Subquery(alias, child) => Some(alias)
+      case _ => None
+    }
+
+  /** Creates an iterator of autoincremental integer IDs. */
+  private def newIds(): Iterator[Int] = Stream.from(1).iterator
+
+  /** Returns a new subquery alias based on an ID generator. */
+  private[this] def newQueryName(ids: Iterator[Int]): String = s"__subquery${ids.next()}"
 
 }
