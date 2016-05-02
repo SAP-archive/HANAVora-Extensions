89c884e1cc70e48dee019481b85fff54f171db7e
Axel Christ <axel.christ@sap.com>
2016-05-02 16:39:36 +0200

    * Create test isolated datasource (VORASPARK-181)
    - Create new com.sap.spark.vora.dsmock datasource
    - Datasource configurable via mocking
    - Create multithreading tests to verify behavior during parallel tests
    
    Change-Id: I5954b77ea331912c9b021743cf85d2687936296a

---
+++ core/src/test/scala/com/sap/spark/dsmock/DefaultSource.scala
@@ -0,0 +1,205 @@
+package com.sap.spark.dsmock
+
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.catalyst.plans.logical.{PersistedDimensionView, PersistedView}
+import org.apache.spark.sql.sources._
+import org.apache.spark.sql.types.{DataType, StructType}
+import org.mockito.Mockito._
+
+import scala.collection.mutable
+
+/**
+  * A mockable data source for spark.
+  *
+  * Caution: Before instantiating this, you need to call
+  * `DefaultSource.withMock`. This will bind an instance of
+  * a mockable default source to your current thread.
+  * Apply [[org.mockito.Mockito]] `when` calls to the underlying field.
+  */
+class DefaultSource
+  extends TemporaryAndPersistentSchemaRelationProvider
+  with TemporaryAndPersistentRelationProvider
+  with PartitionedRelationProvider
+  with RegisterAllTableRelations
+  with ViewProvider
+  with DimensionViewProvider
+  with DatasourceCatalog
+  with PartitioningFunctionProvider {
+
+  val underlying: DefaultSource = DefaultSource.currentMock
+
+  override def createRelation(sqlContext: SQLContext,
+                              parameters: Map[String, String]): BaseRelation =
+    underlying.createRelation(sqlContext, parameters)
+
+  override def createRelation(sqlContext: SQLContext,
+                              parameters: Map[String, String],
+                              schema: StructType): BaseRelation =
+    underlying.createRelation(sqlContext, parameters, schema)
+
+  override def createRelation(sqlContext: SQLContext,
+                              parameters: Map[String, String],
+                              schema: StructType,
+                              isTemporary: Boolean,
+                              allowExisting: Boolean): BaseRelation =
+    underlying.createRelation(sqlContext, parameters, schema, isTemporary, allowExisting)
+
+  override def createRelation(sqlContext: SQLContext,
+                              parameters: Map[String, String],
+                              isTemporary: Boolean,
+                              allowExisting: Boolean): BaseRelation =
+    underlying.createRelation(sqlContext, parameters, isTemporary, allowExisting)
+
+  override def createRelation(sqlContext: SQLContext,
+                              parameters: Map[String, String],
+                              partitioningFunction: Option[String],
+                              partitioningColumns: Option[Seq[String]],
+                              isTemporary: Boolean,
+                              allowExisting: Boolean): BaseRelation  =
+    underlying.createRelation(
+      sqlContext,
+      parameters,
+      partitioningFunction,
+      partitioningColumns,
+      isTemporary,
+      allowExisting)
+
+  override def createRelation(sqlContext: SQLContext,
+                              parameters: Map[String, String],
+                              schema: StructType,
+                              partitioningFunction: Option[String],
+                              partitioningColumns: Option[Seq[String]],
+                              isTemporary: Boolean,
+                              allowExisting: Boolean): BaseRelation =
+    underlying.createRelation(
+      sqlContext,
+      parameters,
+      schema,
+      partitioningFunction,
+      partitioningColumns,
+      isTemporary,
+      allowExisting)
+
+  override def getTableRelation(tableName: String,
+                                sqlContext: SQLContext,
+                                options: Map[String, String]): Option[LogicalPlanSource] =
+    underlying.getTableRelation(tableName, sqlContext, options)
+
+  override def getAllTableRelations(sqlContext: SQLContext,
+                                    options: Map[String, String])
+    : Map[String, LogicalPlanSource] =
+    underlying.getAllTableRelations(sqlContext, options)
+
+  override def dropView(dropViewInput: DropViewInput): Unit =
+    underlying.dropView(dropViewInput)
+
+  override def createView(createViewInput: CreateViewInput[PersistedView]): Unit =
+    underlying.createView(createViewInput)
+
+  override def createDimensionView(createViewInput: CreateViewInput[PersistedDimensionView])
+    : Unit =
+    underlying.createDimensionView(createViewInput)
+
+  override def dropDimensionView(dropViewInput: DropViewInput): Unit =
+    underlying.dropDimensionView(dropViewInput)
+
+  override def getRelation(sqlContext: SQLContext,
+                           name: Seq[String],
+                           options: Map[String, String]): Option[RelationInfo] = {
+    underlying.getRelation(sqlContext, name, options).map { info =>
+      RelationInfo(info.name, info.isTemporary, info.kind, info.ddl)
+    }
+  }
+
+  override def getRelations(sqlContext: SQLContext,
+                            options: Map[String, String]): Seq[RelationInfo] =
+    underlying.getRelations(sqlContext, options).map { info =>
+      RelationInfo(info.name, info.isTemporary, info.kind, info.ddl)
+    }
+
+  override def getTableNames(sqlContext: SQLContext,
+                             parameters: Map[String, String]): Seq[String] =
+    underlying.getTableNames(sqlContext, parameters)
+
+  override def dropPartitioningFunction(sqlContext: SQLContext,
+                                        parameters: Map[String, String],
+                                        name: String,
+                                        allowNotExisting: Boolean): Unit =
+    underlying.dropPartitioningFunction(sqlContext, parameters, name, allowNotExisting)
+
+  override def createRangeSplitPartitioningFunction(sqlContext: SQLContext,
+                                                    parameters: Map[String, String],
+                                                    name: String,
+                                                    datatype: DataType,
+                                                    splitters: Seq[Int],
+                                                    rightClosed: Boolean): Unit =
+    underlying.createRangeSplitPartitioningFunction(
+      sqlContext,
+      parameters,
+      name,
+      datatype,
+      splitters,
+      rightClosed)
+
+  override def createHashPartitioningFunction(sqlContext: SQLContext,
+                                              parameters: Map[String, String],
+                                              name: String,
+                                              datatypes: Seq[DataType],
+                                              partitionsNo: Option[Int]): Unit =
+    underlying.createHashPartitioningFunction(
+      sqlContext,
+      parameters,
+      name,
+      datatypes,
+      partitionsNo)
+
+  override def createRangeIntervalPartitioningFunction(sqlContext: SQLContext,
+                                                       parameters: Map[String, String],
+                                                       name: String,
+                                                       datatype: DataType,
+                                                       start: Int,
+                                                       end: Int,
+                                                       strideParts: Either[Int, Int]): Unit =
+    underlying.createRangeIntervalPartitioningFunction(
+      sqlContext,
+      parameters,
+      name,
+      datatype,
+      start,
+      end,
+      strideParts)
+
+  override def getAllPartitioningFunctions(sqlContext: SQLContext,
+                                           parameters: Map[String, String])
+    : Seq[PartitioningFunction] =
+    underlying.getAllPartitioningFunctions(sqlContext, parameters)
+}
+
+object DefaultSource {
+  private val mocks = new mutable.HashMap[Long, DefaultSource]()
+
+  private def currentMock: DefaultSource = mocks.getOrElse(threadId,
+    throw new IllegalAccessException("You need to call `DefaultSource.withMock` " +
+      "before instantiating DefaultSource objects"))
+
+  private def threadId: Long = Thread.currentThread().getId
+
+  /**
+    * Creates a context for doing operations with a mocked [[DefaultSource]].
+    *
+    * Mocking calls can be directly done on the function parameter object.
+    * All new calls in the created context will return a [[DefaultSource]]
+    * with the same mock as underlying attribute.
+    *
+    * @param op The context of operations
+    * @tparam A The return type of the context
+    * @return The result of the context operation
+    */
+  def withMock[A](op: DefaultSource => A): A = {
+    val preparedMock = mock(classOf[DefaultSource])
+    mocks(threadId) = preparedMock
+    val result = op(preparedMock)
+    mocks.remove(threadId)
+    result
+  }
+}
---
+++ core/src/test/scala/org/apache/spark/MockedDefaultSourceSuite.scala
@@ -0,0 +1,74 @@
+package org.apache.spark
+
+import java.util.concurrent.{Callable, Executors}
+
+import com.sap.spark.dsmock.DefaultSource
+import org.apache.spark.sql.sources.HashPartitioningFunction
+import org.apache.spark.sql.{GlobalSapSQLContext, Row, SQLContext}
+import org.mockito.Matchers._
+import org.mockito.Mockito._
+import org.scalatest.FunSuite
+
+import scala.concurrent.duration._
+
+/**
+  * Test suite to verify behavior of the mockable [[DefaultSource]].
+  */
+class MockedDefaultSourceSuite
+  extends FunSuite
+  with GlobalSapSQLContext {
+
+  val testTimeout = 10 // seconds
+
+  private def numberOfThreads: Int = {
+    val noOfCores = Runtime.getRuntime.availableProcessors()
+    assert(noOfCores > 0)
+
+    if (noOfCores == 1) 2 // It should always be multithreaded although only
+                          // one processor is available (pseudo-multithreading)
+    else noOfCores
+  }
+
+  def runMultiThreaded[A](op: Int => A): Seq[A] = {
+    info(s"Running with $numberOfThreads threads")
+    val pool = Executors.newFixedThreadPool(numberOfThreads)
+
+    val futures = 1 to numberOfThreads map { i =>
+      val task = new Callable[A] {
+        override def call(): A = op(i)
+      }
+      pool.submit(task)
+    }
+
+    futures.map(_.get(testTimeout, SECONDS))
+  }
+
+  test("Underlying mocks of multiple threads are distinct") {
+    val dataSources = runMultiThreaded { _ =>
+      DefaultSource.withMock(identity)
+    }
+
+    dataSources foreach { current =>
+      val sourcesWithoutCurrent = dataSources.filter(_.ne(current))
+      assert(sourcesWithoutCurrent.forall(_.underlying ne current))
+    }
+  }
+
+  test("Mocking works as expected") {
+    runMultiThreaded { i =>
+      DefaultSource.withMock { defaultSource =>
+        when(defaultSource.getAllPartitioningFunctions(
+          anyObject[SQLContext],
+          anyObject[Map[String, String]]))
+          .thenReturn(Seq(HashPartitioningFunction(s"foo$i", Seq.empty, None)))
+
+        val Array(Row(name)) = sqlc
+          .sql("SHOW PARTITIONING FUNCTIONS USING com.sap.spark.dsmock")
+          .select("name")
+          .collect()
+
+        assertResult(s"foo$i")(name)
+      }
+    }
+  }
+}
